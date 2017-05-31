e6c0d58df73f42b67c2c0e1d2719e4bccd1014b9
Aleksandr <aleksandr@asdfjackal.com>
2017-05-30 23:23:56 -0400

    Fixed final eslint errors in server files

--- server/settings.js
+++ server/settings.js
@@ -1,10 +1,10 @@
-function envOrDefault(env, defaultValue){
+function envOrDefault(env, defaultValue) {
   return process.env[env] || defaultValue;
 }
 
-const PORT = envOrDefault("PORT", 4000)
-const JWT_SECRET = envOrDefault("JWT_SECRET", "secret-key")
-const PG_URI = envOrDefault("PG_URI", "postgres://asdfjackal:test@localhost:5432/asdfjackal")
+const PORT = envOrDefault('PORT', 4000);
+const JWT_SECRET = envOrDefault('JWT_SECRET', 'secret-key');
+const PG_URI = envOrDefault('PG_URI', 'postgres://asdfjackal:test@localhost:5432/asdfjackal');
 
 export {
   PORT,
